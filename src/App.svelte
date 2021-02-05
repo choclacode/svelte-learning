dbf1756391fd9ccc608074f8828cdc362fa6d76f
choclacode <saqib@choclacode.eu.org>
2021-02-05 22:24:53 +0600

    Refactored src

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,8 @@
 <script>
   import Layout from './components/layout/Layout.svelte';
-  import Navbar from './components/layout/Navbar.svelte';
   import Poll from './components/poll/Poll.svelte';
 </script>
 
 <Layout>
-  <Navbar />
   <Poll />
-  <!-- <Poll {activeItem} {polls}
-    on:addPoll={({ detail }) => {
-      polls = [...polls, detail];
-      activeItem = 'Current Polls';
-    }}
-    on:vote={({ detail: { choice, id } }) => {
-      const copiedPolls = [...polls];
-      const upvotedPoll = copiedPolls.find(poll => poll.id === id);
-      upvotedPoll.choices[choice].votes++;
-      polls = copiedPolls;
-    }}
-  /> -->
 </Layout>
--- src/components/layout/Layout.svelte
+++ src/components/layout/Layout.svelte
@@ -1,10 +1,12 @@
 <script>
   import Header from './Header.svelte';
+  import Navbar from './Navbar.svelte';
   import Footer from './Footer.svelte';
 </script>
 
 <Header />
 <main>
+  <Navbar />
   <slot></slot>
 </main>
 <Footer />
